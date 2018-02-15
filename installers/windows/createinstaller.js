fd5065dc2c3be09afdb9fda09b4fef702013bfb7
goldminer1030 <goldminer1030@gmail.com>
2018-02-15 20:44:45 +0100

    added build

---
+++ installers/windows/createinstaller.js
@@ -0,0 +1,24 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  console.log('creating windows installer')
+  const rootPath = path.join('./')
+  const outPath = path.join(rootPath, 'release-builds')
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'gelectron-app-win32-ia32/'),
+    authors: 'Gold Miner',
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    exe: 'electron-tutorial-app.exe',
+    setupExe: 'GElectronAppInstaller.exe'
+  })
+}
